e8b595de40544d53fb9a2c25fbab69d578192b52
krystiankornowski <63247660+krystiankornowski@users.noreply.github.com>
2020-04-06 17:43:46 +0200

    Add files via upload

---
+++ php/contact.php
@@ -0,0 +1,72 @@
+<?php
+
+	$errors = array();
+
+	// Check if name has been entered
+	if (!isset($_POST['name'])) {
+		$errors['name'] = 'Wprowadź swoje imię';
+	}
+
+	// Check if email has been entered and is valid
+	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'Wprowadź swój email';
+	}
+
+	//Check if message has been entered
+	if (!isset($_POST['message'])) {
+		$errors['message'] = 'Wprowadź swoją wiadomość';
+	}
+
+	$errorOutput = '';
+
+	if(!empty($errors)){
+
+		$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+ 		$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+
+		$errorOutput  .= '<ul>';
+
+		foreach ($errors as $key => $value) {
+			$errorOutput .= '<li>'.$value.'</li>';
+		}
+
+		$errorOutput .= '</ul>';
+		$errorOutput .= '</div>';
+
+		echo $errorOutput;
+		die();
+	}
+
+
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = $email;
+	$to = 'kornowski.krystian@gmail.com';  // please change this email id
+	$subject = 'Formularz kontaktowy – Krystian Kornowski Branding+Digital';
+
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+	$headers = "From: ".$from;
+
+
+	//send the email
+	$result = '';
+	if (mail ($to, $subject, $body, $headers)) {
+		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
+ 		$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		$result .= 'Dzięki za wiadomość! Odezwę się jak najszybciej.';
+		$result .= '</div>';
+
+		echo $result;
+		die();
+	}
+
+	$result = '';
+	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	$result .= 'Coś poszło nie tak. Spróbuj jeszcze raz.';
+	$result .= '</div>';
+
+	echo $result;
